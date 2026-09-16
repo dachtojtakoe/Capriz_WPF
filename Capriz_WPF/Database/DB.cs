993ea50db45632f83a5208f80a0e27935b3ec8ea
dachtojtakoe <78024557+dachtojtakoe@users.noreply.github.com>
2026-09-16 10:47:08 +0300

    Починен скроллинг таблицы, а также добавлен минимальный порог количества данных для деления данных

--- Capriz_WPF/Database/DB.cs
+++ Capriz_WPF/Database/DB.cs
@@ -705,7 +705,8 @@ namespace Capriz_WPF.Database
                 int hours = diff.Hours;
 
                 int delimeter = /*days > 60 ? 90 : days > 30 ? 54 :*/ days > 7 ? 18 : days > 1 ? 9 : days == 1 ? 3 : hours > 12 ? 2 : 1;
-                if(delimeter != 1)
+                int minRowsForDelimiter = delimeter * 10;
+                if (delimeter != 1)
                 {
                     command.CommandText = $@"
                     WITH NumberedRows AS (
@@ -726,8 +727,9 @@ namespace Capriz_WPF.Database
                     [{column}]
                 FROM 
                     NumberedRows
-                WHERE 
-                    RowNum % {delimeter} = 1
+                WHERE
+                    TotalRows <= {minRowsForDelimiter}
+                    OR RowNum % {delimeter} = 1
                     OR RowNum = TotalRows;";
                 }
                 else
